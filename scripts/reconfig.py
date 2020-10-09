eefbb8b5b564fe79422ff2c640539b2281a38e0c
William Schultz <will627@comcast.net>
2020-10-09 11:19:39 -0400

    Some edits to reconfig script for further analysis

--- scripts/reconfig.py
+++ scripts/reconfig.py
@@ -16,7 +16,7 @@ reconfig_graphs = list()
 last_edges = None
 reconfig_graphs.append(None)
 for k,line in enumerate(lines):
-    if not (line[0]=="{" and line[-1]=="}"):
+    if len(line)<2 or not (line[0]=="{" and line[-1]=="}"):
         print "Skipping line: ", line
         continue
     # print line
@@ -24,20 +24,27 @@ for k,line in enumerate(lines):
         if len(reconfig_graphs)>0 and reconfig_graphs[-1]: 
             reconfig_graphs.append(None)
     else:
-        print "raw,",line
-        cleaned_line = line.strip().replace("{", "").replace("}", "").replace("\"", "")
+        print "raw:",line
+        cleaned_line = line.replace("n1, ", "n1-")
+        cleaned_line = cleaned_line.replace("n2, ", "n2-")
+        cleaned_line = cleaned_line.replace("n3, ", "n3-")
+        cleaned_line = cleaned_line.replace("n4, ", "n4-")
+        cleaned_line = cleaned_line.replace("n5, ", "n5-")
+        cleaned_line = cleaned_line.strip().replace("{", "").replace("}", "").replace("\"", "")
+        print "cleaned:",cleaned_line
         edges = cleaned_line.split(",")
-        print "cleaned,",cleaned_line
         edges = tuple([e.strip() for e in edges])
+        print edges
         def parse_edge(e):
             nodes = e.split("->")
             return (nodes[0].strip(), nodes[1].strip())
-        edges = map(parse_edge, edges)
-        # print edges
+        edges = tuple(map(parse_edge, edges))
+        print edges
         if last_edges and last_edges != edges:
             reconfig_graphs.append(edges)
         last_edges = edges
 reconfig_graphs.append(None)
+reconfig_graphs = set(reconfig_graphs)
 
 # reconfig_graphs = sorted(reconfig_graphs, key = lambda rc : len(rc), reverse=True)
 # reconfig_graphs = reversed(reconfig_graphs)
@@ -60,8 +67,13 @@ for k,rc in enumerate(reconfig_graphs):
         for edge in rc:
             n1, n2 = (edge)
             curr_subgraph.node(str(k)+n1, label=n1)
+            if n1[1] != n2[1]:
+                # curr_subgraph.attr('node', fillcolor='lightblue', style="filled")
+                curr_subgraph.attr('edge', color='red', style="filled")
             curr_subgraph.node(str(k)+n2, label=n2)
             curr_subgraph.edge(str(k)+n1, str(k)+n2)
+            # curr_subgraph.attr('node', fillcolor='none', style="filled")
+            curr_subgraph.attr('edge', color='black')
 
 # dot.render('graphs/config-graph-' + str(k) + '.gv', view=False)
 dot.render('graphs/config-graph-FULL' + '.gv', view=False)
