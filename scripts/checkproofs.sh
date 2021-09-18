5b86fb3e5b702eb4ef7fa281af482af9604eafe1
William Schultz <will627@comcast.net>
2021-09-18 11:05:34 -0400

    Update check script

--- scripts/checkproofs.sh
+++ scripts/checkproofs.sh
@@ -4,13 +4,13 @@
 # Check all TLAPS proofs for establishing safety of MongoRaftReconfig.
 #
 
-tlapm --stretch 3 -v --cleanfp --timing -I ../ AuxLemmas.tla
-tlapm --stretch 3 -v --cleanfp --timing -I ../ BasicQuorumsLib.tla
-tlapm --stretch 3 -v --cleanfp --timing -I ../ ElectionSafetyLemmas.tla
-tlapm --stretch 3 -v --cleanfp --timing -I ../ LeaderCompletenessLemmas.tla
-tlapm --stretch 3 -v --cleanfp --timing -I ../ LeaderCompletenessLib.tla
-tlapm --stretch 3 -v --cleanfp --timing -I ../ Lib.tla
-tlapm --stretch 3 -v --cleanfp --timing -I ../ LogLemmas.tla
-tlapm --stretch 3 -v --cleanfp --timing -I ../ MongoRaftReconfigProofs.tla
-tlapm --stretch 3 -v --cleanfp --timing -I ../ StateMachineSafetyLemmas.tla
-tlapm --stretch 3 -v --cleanfp --timing -I ../ TypeOK.tla
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ AuxLemmas.tla
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ BasicQuorumsLib.tla
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ ElectionSafetyLemmas.tla
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ LeaderCompletenessLemmas.tla
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ LeaderCompletenessLib.tla
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ Lib.tla
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ LogLemmas.tla
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ MongoRaftReconfigProofs.tla
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ StateMachineSafetyLemmas.tla
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ TypeOK.tla
