452be079a42c66cae353a6451380fb5e6c7bf44e
William Schultz <will627@comcast.net>
2021-09-21 18:38:58 -0400

    Update proofstats script

--- scripts/proofstats.sh
+++ scripts/proofstats.sh
@@ -4,22 +4,31 @@
 #
 
 function prooflines {
-    cat AuxLemmas.tla \
-        Axioms.tla \
-        BasicQuorumsLib.tla \
-        ElectionSafetyLemmas.tla \
-        LeaderCompletenessLemmas.tla \
-        LeaderCompletenessLib.tla \
-        Lib.tla \
-        LogLemmas.tla \
-        MongoRaftReconfigIndInv.tla \
-        MongoRaftReconfigProofs.tla \
-        StateMachineSafetyLemmas.tla 
+    cat proofs/AuxLemmas.tla \
+        proofs/Axioms.tla \
+        proofs/BasicQuorumsLib.tla \
+        proofs/ElectionSafetyLemmas.tla \
+        proofs/LeaderCompletenessLemmas.tla \
+        proofs/LeaderCompletenessLib.tla \
+        proofs/Lib.tla \
+        proofs/LogLemmas.tla \
+        proofs/MongoRaftReconfigIndInv.tla \
+        proofs/MongoRaftReconfigProofs.tla \
+        proofs/StateMachineSafetyLemmas.tla \
+        MongoRaftReconfig.tla \
+        MongoLoglessDynamicRaft.tla \
+        MongoStaticRaft.tla
 }
 
-cd proofs
+# cd proofs
 echo "Count lines of non-comment code in TLAPS proofs:"
 prooflines | grep -v "\*" | wc -l
+
+echo "Count lines of non-comment code for inductive invariant:"
+cat proofs/MongoRaftReconfigIndInv.tla | grep -v "\*" | wc -l
+
+echo "Count lines of non-comment code for protocol spec:"
+cat MongoRaftReconfig.tla MongoLoglessDynamicRaft.tla MongoStaticRaft.tla | grep -v "\*" | wc -l
 
 echo "Count LEMMA statements in TLAPS proofs:"
 prooflines | grep "LEMMA" | wc -l
