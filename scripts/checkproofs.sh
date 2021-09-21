b3ec6b962008de99c6e74c77495c53ff57c9901f
William Schultz <will627@comcast.net>
2021-09-21 13:17:13 -0400

    Update proof checking script

--- scripts/checkproofs.sh
+++ scripts/checkproofs.sh
@@ -4,13 +4,21 @@
 # Check all TLAPS proofs for establishing safety of MongoRaftReconfig.
 #
 
-tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ AuxLemmas.tla
-tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ BasicQuorumsLib.tla
-tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ ElectionSafetyLemmas.tla
-tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ LeaderCompletenessLemmas.tla
-tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ LeaderCompletenessLib.tla
-tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ Lib.tla
-tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ LogLemmas.tla
-tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ MongoRaftReconfigProofs.tla
-tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ StateMachineSafetyLemmas.tla
-tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ TypeOK.tla
+echo "" > checkproofs.log
+
+# Check all proofs.
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ AuxLemmas.tla 2>&1 | tee -a checkproofs.log
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ BasicQuorumsLib.tla 2>&1 | tee -a checkproofs.log
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ ElectionSafetyLemmas.tla 2>&1 | tee -a checkproofs.log
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ LeaderCompletenessLemmas.tla 2>&1 | tee -a checkproofs.log
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ LeaderCompletenessLib.tla 2>&1 | tee -a checkproofs.log
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ Lib.tla 2>&1 | tee -a checkproofs.log
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ LogLemmas.tla 2>&1 | tee -a checkproofs.log
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ MongoRaftReconfigProofs.tla 2>&1 | tee -a checkproofs.log
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ StateMachineSafetyLemmas.tla 2>&1 | tee -a checkproofs.log
+tlapm --toolbox 0 0 --stretch 3 -v --cleanfp --timing -I ../ TypeOK.tla 2>&1 | tee -a checkproofs.log
+
+# Check for failure to prove any proof obligations.
+echo "========================================"
+echo "TLAPS Proof obligation failures:"
+grep -B 3 -A 3 "status:failed" checkproofs.log
