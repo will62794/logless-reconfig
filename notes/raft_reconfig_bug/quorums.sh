21cd752ed71f24f1865a042fddba98a431db79e0
William Schultz <will627@comcast.net>
2024-02-18 01:05:23 -0500

    save neato renders

--- notes/raft_reconfig_bug/quorums.sh
+++ notes/raft_reconfig_bug/quorums.sh
@@ -7,14 +7,24 @@ do
   $tlc -noGenerateSpecTE -config $name.cfg -dump dot,colorize $name.dot quorums
   sed -E -i "" "s/.*rank.*;}//" $name.dot
   sed -E -i "" "s/.*rank.*;}//" $name.dot
-  sed -E -i "" "s/nodesep=0.35;/nodesep=0.35;concentrate=false;/" $name.dot
   # Render standard.
-  dot -Tpng -Gdpi=200 -Gconcentrate=true $name.dot > $name.png
+  dot -Tpng -Gdpi=180 -Gconcentrate=true $name.dot > $name.png
 
   # Remove the legend for fdp.
   sed -E -i "" "s/SingleNodeChange.*//" $name.dot
   sed -E -i "" "s/ToQuorumOverlap.*//" $name.dot
 
   # Render with force-directed.
-  dot -Tpng -Ksfdp -Gdpi=180 -Gnodesep=0.065 -Gconcentrate=false $name.dot > ${name}_fdp.png
+  # command line flag to remove all arrowheads: 
+  # Set edge style from command line flag: 
+
+  nodesep="0.01"
+  # If "quorums_n2" then make nodesep=0.3
+  if [ $name = "quorums_n2" ]
+  then
+      nodesep="0.35"
+  fi
+
+  dot -Tpng -Kneato -Gdpi=180 -Gnodesep=$nodesep -Epenwidth=0.7 -Goverlap=scale -Gconcentrate=false $name.dot > ${name}_neato.png
+  dot -Tpng -Ksfdp -Gdpi=180 -Gnodesep=$nodesep -Epenwidth=0.7 -Goverlap=scale -Gconcentrate=false $name.dot > ${name}_fdp.png
 done
